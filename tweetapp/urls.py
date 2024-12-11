d1b6e4122bcde8254d6f29e42405a60a54853271
Yusuf Kaleli <yusuf.kaleli90@gmail.com>
2024-12-11 23:04:33 +0300

    first commit

---
+++ tweetapp/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+app_name = 'tweetapp'
+
+urlpatterns = [
+    path('',views.listtweet,name='listtweet'),
+    path('addtweet/',views.addtweet,name='addtweet'),
+    path('addtweetbyform', views.addtweetbyform,name='addtweetbyform'),
+    path('addtweetbymodelform', views.addtweetbymodelform,name='addtweetbymodelform'),
+    path('signup/',views.SignUpView.as_view(),name="signup"),
+    path("deletetweet/<int:id>",views.deletetweet, name="deletetweet")
+    
+]
